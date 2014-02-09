97c9c738d7f7b12987bc016f3304110536d6f846
Albert Stepantsev <philfromfmf>
2014-02-09 19:51:42 +0400

    t4 application is created

---
+++ protected/config.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'db' => [
+        'default' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'dbname' => 'kdm',
+            'user' => 'root',
+            'password' => '',
+        ]
+    ],
+    'extensions' => [
+        'jquery' => [
+        ],
+        'bootstrap' => [
+        ],
+    ],
+];

7a3fab2732fc8a8332fc29e3796dedb0cb187dce
Albert Stepantsev <philfromfmf>
2014-04-15 23:17:09 +0400

    Синхронизация с T4

--- protected/Controllers/Index.php
+++ protected/Controllers/Index.php
@@ -17,19 +17,20 @@ class Index
         $this->data->content->text = 'This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.';
     }
 
-    public function actionLogin($email=null, $password=null)
+    public function actionLogin($email=null, $password=null, $return='/')
     {
         $this->data->error = $this->app->flash->error;
         if (!empty($email) && !empty($password)) {
             try {
                 $identity = new Identity();
                 $identity->authenticate(new Std(['email'=>$email, 'password'=>$password]));
-                $this->redirect('/');
+                $this->redirect($return);
             } catch (Exception $e) {
                 $this->app->flash->error = $e->getMessage();
             }
         }
-        $this->data->email = $email;
+        $this->data->email  = $email;
+        $this->data->return = $return;
     }
 
     public function actionLogout()

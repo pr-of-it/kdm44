8b24feccf8de56ca76426326847e9297149583d2
Albert Stepantsev <philfromfmf@mail.ru>
2014-04-17 13:55:45 +0400

    Fix адреса в новостях

--- protected/routes.php
+++ protected/routes.php
@@ -3,6 +3,6 @@
 return [
     'index' => '///',
 
-    'news/topic/<1>' => '/News/Index/NewsByTopic(id=<1>)',
+    'news/topics/<1>' => '/News/Index/NewsByTopic(id=<1>)',
     'news/<1>' => '/News/Index/Story(id=<1>)',
 ];

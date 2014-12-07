366aa394855f9cabbf3805b3060c0f62ec4868dc
Albert Stepantsev <philfromfmf@mail.ru>
2014-12-07 22:07:27 +0300

    + New layout is added

--- protected/routes.php
+++ protected/routes.php
@@ -2,15 +2,11 @@
 
 return [
 
-    'index' => '///',
+    '/index' => '///',
 
-    'news/admin' => '/News/Admin/',
-    'news/admin/<1>' => '/News/Admin/<1>',
-    'news/topics/<1>' => '/News/Index/NewsByTopic(id=<1>)',
-    'news/<1>' => '/News/Index/Story(id=<1>)',
+    '/news/topics/<1>' => '/News/Index/NewsByTopic(id=<1>)',
+    '/news/<1>' => '/News/Index/Story(id=<1>)',
 
-    'pages/admin' => '/Pages/Admin/',
-    'pages/admin/<1>' => '/Pages/Admin/<1>',
-    'pages/<1>' => '/Pages/Index/PageByUrl(url=<1>)',
+    '/pages/<1>' => '/Pages/Index/PageByUrl(url=<1>)',
 
 ];

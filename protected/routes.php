b1686518a8fc36b226f4670fc1f0eeb822ccc5e8
Albert Stepantsev <philfromfmf@mail.ru>
2014-12-22 22:20:09 +0300

    ! решение проблемы со старыми "длинными" урлами

--- protected/routes.php
+++ protected/routes.php
@@ -7,6 +7,8 @@ return [
     '/news/topics/<1>' => '/News/Index/NewsByTopic(id=<1>)',
     '/news/<1>' => '/News/Index/Story(id=<1>)',
 
+    '/pages/<3>/<2>/<1>' => '/Pages/Index/PageByUrl(url=<1>)',
+    '/pages/<2>/<1>' => '/Pages/Index/PageByUrl(url=<1>)',
     '/pages/<1>' => '/Pages/Index/PageByUrl(url=<1>)',
 
 ];

9d45e6fc5170eba7ca6959329e505557d8ab1d00
barricade <shilov.kirill.transas@gmail.com>
2015-04-16 22:44:43 +0300

    UploadImages model created

---
+++ protected/Modules/News/Models/Images.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: barricade
+ * Date: 16.04.15
+ * Time: 22:29
+ */
+
+namespace App\Modules\News\Models;
+
+
+class Images extends Model
+{
+    static protected $schema=[
+        'table' => 'newsimages',
+        'columns' => [
+            'image' => ['type'=>'string']
+        ],
+        'relations' => [
+            'story' => ['type'=>self::BELONGS_TO, 'model'=>'App\Modules\News\Models\Story'],
+        ]
+    ];
+}

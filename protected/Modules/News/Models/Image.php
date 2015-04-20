e76d40ebb21e9558358197078d09997ef8b88a9e
barricade <shilov.kirill.transas@gmail.com>
2015-04-20 23:05:32 +0300

    Created methids for Image upload

--- protected/Modules/News/Models/Image.php
+++ protected/Modules/News/Models/Image.php
@@ -26,4 +26,74 @@ class Image extends Model
             'story' => ['type'=>self::BELONGS_TO, 'model'=>'\App\Modules\News\Models\Story'],
         ]
     ];
+
+    public function uploadImage($formFieldName)
+    {
+        $request = Application::getInstance()->request;
+        if (!$request->existsFilesData() || !$request->isUploaded($formFieldName) || $request->isUploadedArray($formFieldName))
+            return $this;
+
+        try {
+            $uploader = new Uploader($formFieldName);
+            $uploader->setPath('/public/news/stories/images');
+            $image = $uploader();
+            if ($this->path)
+                $this->deleteImage();
+            $this->path = $image;
+        } catch (Exception $e) {
+            $this->path = null;
+        }
+        return $this;
+    }
+
+    public function uploadFiles($formFieldName)
+    {
+        $request = Application::getInstance()->request;
+        if (!$request->existsFilesData() || !$request->isUploadedArray($formFieldName))
+            return $this;
+
+        $uploader = new Uploader($formFieldName);
+        $uploader->setPath('/public/news/stories/images');
+        print_r($this->images);
+        foreach ($uploader() as $uploadedFilePath) {
+            if (false !== $uploadedFilePath)
+                $this->images->append(new Image(['path' => $uploadedFilePath]));
+        }
+        return $this;
+    }
+
+    public function beforeDelete()
+    {
+        $this->deleteImage();
+        $this->deleteFiles();
+        return parent::beforeDelete();
+    }
+
+    public function deleteImage()
+    {
+        if ($this->path) {
+            try {
+                $this->path = '';
+                Helpers::removeFile(ROOT_PATH_PUBLIC . $this->path);
+            } catch (\T4\Fs\Exception $e) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function deleteFiles()
+    {
+        if (!empty($this->path)) {
+            try {
+                $this->images = new Collection();
+                foreach ($this->images as $image) {
+                    Helpers::removeFile(ROOT_PATH_PUBLIC . $image->path);
+                }
+            } catch (\T4\Fs\Exception $e) {
+                return false;
+            }
+        }
+        return true;
+    }
 }

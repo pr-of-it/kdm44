128f9843cf0e4e4aca8d4ff0a4e0d3a79173de5c
ryzhikk <dianakrylowa@yandex.ru>
2015-04-20 14:46:10 +0300

    Сохранение настроек

--- protected/Modules/Admin/Controllers/Settings.php
+++ protected/Modules/Admin/Controllers/Settings.php
@@ -3,7 +3,10 @@
 namespace App\Modules\Admin\Controllers;
 
 use T4\Core\Config;
+use T4\Core\Std;
 use T4\Mvc\Controller;
+use T4\Http\Uploader;
+use T4\Core\Collection;
 
 class Settings
     extends Controller
@@ -26,6 +29,15 @@ class Settings
             $config = new Config();
         }
         $config->setPath(ROOT_PATH_PROTECTED . '/settings.php');
+
+        if ($this->app->request->existsFilesData() || $this->app->request->isUploadedArray('files')) {
+            $uploader = new Uploader('files');
+            $uploader->setPath('/public/settings/slider');
+            foreach ($uploader() as $uploadedFilePath) {
+                if (false !== $uploadedFilePath)
+                    $this->app->request->post->settings->slider->merge(new Std([count($this->app->request->post->settings->slider) => ['src' => $uploadedFilePath, 'link' => '']]));
+            }
+        }
         $config->merge($this->app->request->post->settings);
         $config->save();
         $this->redirect('/admin/settings');

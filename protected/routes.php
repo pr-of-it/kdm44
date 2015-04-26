ebc8b737382df4704342c2a5d50d15956fd9eee3
vitaliy_eropkin <ist70@mail.ru>
2015-04-26 23:28:33 +0300

    Изменение route

--- protected/routes.php
+++ protected/routes.php
@@ -4,7 +4,12 @@ return [
 
     '/index' => '///',
 
+    '/news/archive/topic/<1>/<2>/<3>' => '/News/Index/ArchiveMonthByTopic(year=<1>,month=<2>,id=<3>)',
+    '/news/archive/<1>/<2>' => '/News/Index/ArchiveByMonth(year=<1>,month=<2>)',
     '/news/topics/<1>' => '/News/Index/NewsByTopic(id=<1>)',
+    '/news/archive/<1>' => '/News/Index/Archive(year=<1>)',
+    '/news/archives/<1>' => '/News/Index/Archives(id=<1>)',
+//    '/news/topics/<1>' => '/News/Index/NewsByTopic(id=<1>)',
     '/news/<1>' => '/News/Index/Story(id=<1>)',
 
     '/pages/<3>/<2>/<1>' => '/Pages/Index/PageByUrl(url=<1>)',

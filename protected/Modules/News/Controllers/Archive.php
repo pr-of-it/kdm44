a0cf2ce9b2d9606b0515c7bd6cac5fae1417b568
alexx-gsm <alexx-gsm@yandex.ru>
2016-07-25 19:49:37 +0700

    add exceptions to archive controllers, rename Story model methods

--- protected/Modules/News/Controllers/Archive.php
+++ protected/Modules/News/Controllers/Archive.php
@@ -3,6 +3,7 @@
 namespace App\Modules\News\Controllers;
 
 use App\Modules\News\Models\Story;
+use T4\Http\E404Exception;
 use T4\Mvc\Controller;
 
 class Archive
@@ -10,15 +11,20 @@ class Archive
 {
     public function actionDefault()
     {
-        $this->data->items = Story::getYears();
+        $this->data->items = Story::getItemsCountGroupByYears();
     }
 
     public function actionNewsByMonth($year = null)
     {
-        if (null !== $year) {
-            $this->data->items = Story::getMonths($year);
+        $date = \DateTime::createFromFormat('Y', $year);
+        if (null === $year || !$date) {
+            throw new E404Exception;
         }
-        
+        if ($year !== $date->format('Y')) {
+            throw new E404Exception;
+        }
+
+        $this->data->items = Story::getItemsCountGroupByMonths($year);
         $this->data->year = $year;
     }
 

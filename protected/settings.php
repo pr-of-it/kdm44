62fc13f44cb435e909de20a0f90833b63f73dd51
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2017-06-30 15:25:40 +0700

    added settings.php

---
+++ protected/settings.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+  'title' => 'Молодёжь Костромской области',
+  'slider' =>
+  [
+    0 =>
+    [
+      'src' => '/public/settings/slider/sto.jpg',
+      'link' => '',
+    ],
+    1 =>
+    [
+      'src' => '/public/settings/slider/marafon2.jpg',
+      'link' => '',
+    ],
+    2 =>
+    [
+      'src' => '/public/settings/slider/unarmia_2017.jpg',
+      'link' => '',
+    ],
+    3 =>
+    [
+      'src' => '/public/settings/slider/belij_cvetok_2017.jpg',
+      'link' => '',
+    ],
+    4 =>
+    [
+      'src' => '/public/settings/slider/den_rossii.jpg',
+      'link' => '',
+    ],
+    5 =>
+    [
+      'src' => '/public/settings/slider/88888.jpg',
+      'link' => '',
+    ],
+    6 =>
+    [
+      'src' => '/public/settings/slider/den_sela_2017.jpg',
+      'link' => '',
+    ],
+    7 =>
+    [
+      'src' => '/public/settings/slider/svecha_pamjati.jpg',
+      'link' => '',
+    ],
+    8 =>
+    [
+      'src' => '/public/settings/slider/den_molodezhi_2107.jpg',
+      'link' => '',
+    ],
+  ],
+];

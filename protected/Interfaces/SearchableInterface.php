c352cd29f44832c255abed593889b42efdcebef0
Sergey Kozlov <sergey.kozlov@ucann.ru>
2018-12-12 13:58:45 +0300

    KDM-2 chore: дополнен интерфейс

--- protected/Interfaces/SearchableInterface.php
+++ protected/Interfaces/SearchableInterface.php
@@ -3,14 +3,39 @@
 namespace App\Interfaces;
 
 /**
+ * Интерфейс поиска
+ *
  * Interface SearchableInterface
  * @package App\Interfaces
  */
 interface SearchableInterface
 {
     /**
+     * Поиск по ключевым словам
+     *
      * @param string $string
-     * @return mixed
+     * @return SearchableInterface
      */
-    public static function search(string $string);
+    public static function search(string $string): SearchableInterface;
+
+    /**
+     * Получение заголовка
+     *
+     * @return string
+     */
+    public function getTitle(): string;
+
+    /**
+     * Получение лида
+     *
+     * @return string
+     */
+    public function getLead(): string;
+
+    /**
+     * Получение URL
+     *
+     * @return string
+     */
+    public function getUrl(): string;
 }

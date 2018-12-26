88919782a5c8498f5fdae2788d6a775fc5f13c86
Sergey Kozlov <sergey.kozlov@ucann.ru>
2018-12-26 15:06:40 +0300

    KDM-3 chore: добавил в миграции изменение __prt = 0 на __prt = NULL

--- protected/Migrations/m_1545822990_fixPagesTreeColumns.php
+++ protected/Migrations/m_1545822990_fixPagesTreeColumns.php
@@ -11,10 +11,12 @@ class m_1545822990_fixPagesTreeColumns
     public function up()
     {
         $this->db->execute('ALTER TABLE `pages` CHANGE `__prt` `__prt` BIGINT UNSIGNED NULL;');
+        $this->db->execute('UPDATE `pages` set `__prt`= NULL WHERE `__prt` = 0;');
     }
 
     public function down()
     {
+        $this->db->execute('UPDATE `pages` set `__prt`= 0 WHERE `__prt` IS NULL;');
         $this->db->execute('ALTER TABLE `pages` CHANGE `__prt` `__prt` BIGINT UNSIGNED NOT NULL DEFAULT 0;');
     }
 }

895077d0d57745b31ba13768d85abf262cdf263b
Sergey Kozlov <sergey.kozlov@ucann.ru>
2019-01-17 14:09:19 +0300

    KDM-14 feature: дополнен контроллер

--- protected/Controllers/Search.php
+++ protected/Controllers/Search.php
@@ -6,6 +6,7 @@ use App\Modules\Gallery\Models\Album;
 use App\Modules\News\Models\Story;
 use App\Modules\Pages\Models\Page;
 use T4\Mvc\Controller;
+use T4\Orm\ModelDataProvider;
 
 /**
  * Class Search
@@ -22,6 +23,11 @@ class Search extends Controller
         if (null === $query || '' === $query) {
             return;
         }
+        $page = 1;
+        $this->data->page = $this->app->request->get->page ?: 1;
+
+        $this->data->provider = new ModelDataProvider(Story::class);
+        $this->data->page = $page;
 
         $this->data->stories = Story::search($query);
         $this->data->pages = Page::search($query);

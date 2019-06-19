ebb0aa63bf8400f201b7fd1f3dac8d3e0778dd84
Sergey Kozlov <sergey.kozlov@ucann.ru>
2019-06-19 15:41:43 +0300

    KDM-56 chore: дополнил UserUpdateForm

--- protected/Forms/UserUpdateForm.php
+++ protected/Forms/UserUpdateForm.php
@@ -4,10 +4,13 @@ namespace App\Forms;
 
 use App\Dto\Validation\Validators\CompareValuesValidator;
 use App\Dto\Validation\Validators\MinimalLengthAndHasDigitsValidator;
+use App\Dto\Validation\Validators\PhoneNumberValidator;
 use Runn\Fs\File;
 use Runn\Html\Form\Buttons\SubmitButton;
 use Runn\Html\Form\Fields\PasswordField;
+use Runn\Html\Form\Fields\TextField;
 use Runn\Html\Form\Form;
+use Runn\Validation\Validators\StringValidator;
 
 /**
  * Class UserUpdateForm
@@ -27,6 +30,31 @@ class UserUpdateForm extends Form
     }
 
     protected static $schema = [
+        'firstName' => [
+            'class' => TextField::class,
+            'title' => 'Имя',
+            'validator' => StringValidator::class
+        ],
+        'middleName' => [
+            'class' => TextField::class,
+            'title' => 'Отчество',
+            'validator' => StringValidator::class
+        ],
+        'lastName' => [
+            'class' => TextField::class,
+            'title' => 'Фамилия',
+            'validator' => StringValidator::class
+        ],
+        'organization' => [
+            'class' => TextField::class,
+            'title' => 'Организация',
+            'validator' => StringValidator::class
+        ],
+        'phone' => [
+            'class' => TextField::class,
+            'title' => 'Номер телефона',
+            'validator' => PhoneNumberValidator::class
+        ],
         'password' => [
             'class' => PasswordField::class,
             'title' => 'Введите новый пароль',

8b28ecaf4da67d94891477b0414000343e4d69d7
Sergey Kozlov <sergey.kozlov@ucann.ru>
2019-06-21 17:45:44 +0300

    KDM-59 feature: добавил контроллер Processing, шаблоны Templates/Processing/Default.html, Edit.html

---
+++ protected/Controllers/Processing.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Components\Auth\Identity;
+use App\Models\Recourse;
+use T4\Mvc\Controller;
+
+/**
+ * Обработка обращений
+ *
+ * Class Processing
+ * @package App\Controllers
+ */
+class Processing extends Controller
+{
+    const DEFAULT_STATMENTS_COUNT = 5;
+
+    /**
+     * Отображение списка всех обращений
+     *
+     * @param int $count
+     */
+    public function actionDefault($count = self::DEFAULT_STATMENTS_COUNT)
+    {
+        if (!empty($this->app->user) &&  $this->app->user->hasRole('committeeCollaborator')) {
+            $this->data->page = $this->app->request->get->page ?: 1;
+            $this->data->total = Recourse::countAll();
+            $this->data->size = $count;
+            $this->data->user = $this->app->user;
+
+            $recourses = Recourse::findAll(
+                [
+                    'order' => 'created_at DESC',
+                    'offset' => ($this->data->page - 1) * $count,
+                    'limit' => $count,
+                ]
+            );
+
+            $this->data->items = $recourses;
+
+        } else {
+            $this->redirect('/signIn');
+        }
+    }
+
+    /**
+     * Профиль пользователя
+     */
+    public function actionEdit($id = null)
+    {
+        if (!empty($this->app->user) &&  $this->app->user->hasRole('committeeCollaborator')) {
+            $this->data->user = $this->app->user;
+            $this->data->item = Recourse::find(['__id' => $id]);
+
+
+        } else {
+            $this->redirect('/signIn');
+        }
+    }
+
+    /**
+     * Выход из личного кабинета
+     *
+     * @return void
+     */
+    public function actionLogout(): void
+    {
+        if (!empty($this->app->user)) {
+            (new Identity())->logout();
+        }
+        $this->redirect('/reception');
+    }
+}
